3d19e4e12db1dcb485b5cd3350a65f8bbd0e927e
Noor Almomani <www.noor.b.almomani2@gmail.com>
2023-05-22 21:15:38 +0300

    edit

--- src/PalandromicSubstring.java
+++ src/PalandromicSubstring.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 class PalindromicSubstring {
-    public static String PalindromicSubstring2(String str){
+    public static String PalindromicSubstring2(String strd){
         int left = 0, right = 0 ;
         String s = "";
         String s3 = "";
