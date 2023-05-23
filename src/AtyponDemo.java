38fafbc4df92d86acc97de1b394d98fd8a4827b7
Noor Almomani <www.noor.b.almomani2@gmail.com>
2023-05-23 22:53:24 +0300

    Noor

--- src/AtyponDemo.java
+++ src/AtyponDemo.java
@@ -1,9 +1,8 @@
 public class AtyponDemo {
 
     public static void main(String[] args){
-        System.out.println("Demo1");
-        System.out.println("Demo2");
-        System.out.println("Hi Noor");
+        //Hi I'm Noor
+        System.out.println("Noor");
 
     }
 }
