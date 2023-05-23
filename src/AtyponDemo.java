c8c83c0dbd61ad87098bf76c7e5357ce75e3e650
Noor Almomani <Sgalabed17@cit.just.edu.jo>
2023-05-23 22:19:15 +0300

    add2

--- src/AtyponDemo.java
+++ src/AtyponDemo.java
@@ -4,6 +4,6 @@ public class AtyponDemo {
         System.out.println("Demo1");
         System.out.println("Demo2");
         System.out.println("Hi Noor");
-
+        System.out.println("Hi ");
     }
 }
